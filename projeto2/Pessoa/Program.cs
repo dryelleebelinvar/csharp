071a3b326e5d791efbf61c56fe6a90bf7858d5a9
unknown <dryelle.silva@varsolutions.com>
2023-01-17 16:53:00 -0300

    projeto

---
+++ projeto2/Pessoa/Program.cs
@@ -0,0 +1,41 @@
+﻿//receber nome da pessoa -ok
+//receber a data de nascimento (calculo de idade) -ok
+//receber a altura e peso (calculo do imc) -ok
+//receber salário bruto (calculo do desconto clt)
+//receber o saldo da conta (calcular valor em dólar)
+
+using System;
+
+//projeto
+namespace Pessoa
+{
+    //classe
+    internal class Program 
+    {
+        //método = ação
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Digite o seu nome: ");  //captura da ação
+            var nomePessoa = Console.ReadLine();  //variável + nome da variável  //=valor  //lê a linha e guarda o valor na variável
+
+            Console.WriteLine("Digite sua data de nascimento: ");
+            var dataNascimentoPessoa = Console.ReadLine();
+            var dataNascimento = Convert.ToDateTime(dataNascimentoPessoa);   //conversão de dados
+
+            var anoAtual = DateTime.Now.Year;   //ano atual
+            var idade = anoAtual - dataNascimento.Year;   //calculo
+
+            Console.WriteLine("Digite sua altura: ");  //com .
+            var altura = Convert.ToDecimal(Console.ReadLine());   //conversão de dados
+            Console.WriteLine("Digite seu peso: ");
+            var peso = Convert.ToDecimal(Console.ReadLine());
+
+            //+ - * /
+            var imc = Math.Round((peso / (altura * altura)) * 10000), 2);   //calculo imc + conversão para valor real
+
+            Console.WriteLine("O seu nome é: " + nomePessoa);
+            Console.WriteLine("Você tem " + idade + " anos");
+            Console.WriteLine("Seu IMC é " + imc);
+        }
+    }
+}
