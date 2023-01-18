e5e27fde807659cf2ff0e4968a6cfa601b25a680
unknown <dryelle.silva@varsolutions.com>
2023-01-18 16:34:10 -0300

    update

---
+++ projeto3/OlaMundo/Controllers/OlaMundoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OlaMundo.Controllers
+{
+    //controller - porta de entrada da api
+    [ApiController]
+    [Route("[controller]")]
+    public class OlaMundoController : ControllerBase
+    {
+        public OlaMundoController(ILogger<OlaMundoController> logger)
+        {
+           
+        }
+
+        //método get
+        [HttpGet]
+        //padrão de declaração de método:
+        //visibilidade ou nivel de acesso
+        //retorno
+        public OlaMundo ObterMensagem()
+        {
+           var retorno = new OlaMundo();  //instanciar a classe
+           retorno.Mensagem = "Olá mundo - Essa é a minha primeira Web API!";
+           return retorno;
+        }
+    }
+}
