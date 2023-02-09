9f71f1ccc7e258f4a71f1b19a60d0aab70a03408
unknown <dryelle.silva@varsolutions.com>
2023-02-09 16:12:10 -0300

    update

--- projeto4/FrontPessoa/script.js
+++ projeto4/FrontPessoa/script.js
@@ -1,4 +1,26 @@
-function chamarApiPessoa()
+function processar() //token
+{
+    var requestAutenticacao = {  //requisição
+        "Username": $("#txt_usuario").val(),
+        "Password": $("#txt_senha").val()
+    }
+    $.ajax({  //chamada ajax
+        url: "http://localhost:5438/Autenticacao",
+        type: "POST",
+        data: JSON.stringify(requestAutenticacao), 
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            processarDadosPessoa(response.token)
+            console.log(response.token)
+        },
+        error: function (request, message, error) {
+            alert("Erro ao se autenticar!")
+        }
+    })
+}
+
+function processarDadosPessoa(token) 
 {
     var request = {  //json
         "Nome": $("#txt_nome").val(),
@@ -9,12 +31,14 @@ function chamarApiPessoa()
         "Saldo": parseFloat($("#txt_saldo").val())
     }
     //chamada backend
+    console.log(token)
     $.ajax({
         url: "http://localhost:5438/Pessoa",
         type: "POST",
-        data: JSON.stringify(request),  //pega o objeto request e transforma em um json
+        data: JSON.stringify(request), 
         contentType: "application/json",
-        dataType: "json",
+        dataType: "JSON",
+        headers: {"Authorization": "Bearer " + token},
         success: function (resposta) {
             $("#resultado_nome").text("Nome: " + resposta.nome)
             $("#resultado_idade").text("Idade: " + resposta.idade)
